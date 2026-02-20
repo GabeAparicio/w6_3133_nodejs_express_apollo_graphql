800932dc033c62517c395dad3c10e24cfff13c4f
Gabriel Aparicio <195454686+GabeAparicio@users.noreply.github.com>
2026-02-20 16:34:26 -0500

    Added resolvers to be able to run code

--- resolvers/resolvers.js
+++ resolvers/resolvers.js
@@ -1,3 +1,43 @@
 import MovieModel from '../models/movie.js';
 
-// Resolvers define the technique for fetching the types defined in the schema.
+const movieResolvers = {
+    Query: {
+        // Get all movies
+        getAllMovies: async () => {
+            return await MovieModel.find();
+        },
+
+        // Get movie by ID
+        getMovieById: async (_, { id }) => {
+            return await MovieModel.findById(id);
+        },
+
+        // Get movies by director name (using static method later)
+        getMoviesByDirector: async (_, { director_name }) => {
+            return await MovieModel.findByDirectorName(director_name);
+        }
+    },
+
+    Mutation: {
+        // Insert new movie
+        insertMovie: async (_, { movie }) => {
+            return await MovieModel.create(movie);
+        },
+
+        // Update movie
+        updateMovie: async (_, { id, movie }) => {
+            return await MovieModel.findByIdAndUpdate(id, movie, {
+                new: true,
+                runValidators: true
+            });
+        },
+
+        // Delete movie by ID
+        deleteMovieById: async (_, { id }) => {
+            const result = await MovieModel.findByIdAndDelete(id);
+            return !!result;
+        }
+    }
+};
+
+export default movieResolvers;
